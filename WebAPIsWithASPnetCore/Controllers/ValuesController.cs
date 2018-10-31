6396e8c388e48d1636b04ee9a7b2491c551e7e09
dangdinhsi <siddd00474@fpt.edu.vn>
2018-10-31 22:03:34 +0700

    Building Web APIs with ASP.NET Core

---
+++ WebAPIsWithASPnetCore/Controllers/ValuesController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAPIsWithASPnetCore.Controllers
+{
+    [Route("api/[controller]")]
+
+    public class ValuesController : ControllerBase
+    {
+        // GET: api/<controller>
+        [HttpGet]
+        [Produces("application/json")]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id,string query)
+        {
+            return Ok(new Value{Id = id,Text="value"+id});
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public IActionResult Post([FromBody]Value value)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            //Save the value to the DB
+
+            return CreatedAtAction("Get", new {id = value.Id}, value);
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+
+    public class Value
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+    }
+}
